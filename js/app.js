bcebce7adeac45940ad3422c47cea9541952caf5
hennigk <kayla.hennig@gmail.com>
2015-10-09 22:08:19 +0000

    buttons are more functional

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ var API_URL = "https://loopback-rest-api-demo-ziad-saab.c9.io/api";
 
 // Get a reference to the <div id="app">. This is where we will output our stuff
 var $app = $('#app');
+var $buttons = $("#buttons")
 var $next = $("#next");
 var $prev = $("#previous");
 
@@ -18,13 +19,11 @@ function getAddressBook(id) {
     return $.getJSON(API_URL + '/AddressBooks/' + id);
 }
 
-function getEntries(addressBookId) {
-    // TODO...
-    return $.getJSON(API_URL + "/Entries?filter[where][addressBookId]=" + addressBookId + "&filter[order]=lastName%20ASC&filter[limit]=5");
+function getEntries(addressBookId, offset) {
+    return $.getJSON(API_URL + "/Entries?filter[where][addressBookId]=" + addressBookId + "&filter[order]=lastName%20ASC&filter[limit]=5&filter[skip]=" + offset);
 
 }
 
-console.log(getEntries(2));
 
 function getEntry(entryId) {
 
@@ -40,6 +39,7 @@ function displayAddressBooksList(skip) {
     getAddressBooks(skip).then(
         function(addressBooks) {
             // console.log(addressBooks);
+            
             $app.html(''); // Clear the #app div
 
 
@@ -54,28 +54,31 @@ function displayAddressBooksList(skip) {
 
             $app.find('li').on('click', function() {
                 var addressBookId = $(this).data('id');
-                displayAddressBookEntries(addressBookId);
+                displayAddressBookEntries(addressBookId, 0);
+                AddressBookEntriesButtons(addressBookId);
             });
         }
     )
 }
 
-function displayAddressBookEntries(addressBookId) {
-    getEntries(addressBookId).then(
+function displayAddressBookEntries(addressBookId, offset) {
+    getEntries(addressBookId, offset).then(
         function(entries) {
 
             $app.html(''); // Clear the #app div
             $app.append('<h2>Entries</h2>');
             $app.append('<ul>');
-console.log(entries)
+            console.log(entries)
             entries.forEach(function(ent) {
                 console.log(ent)
-                $app.find('ul').append('<li data-id="' + ent.id + '">' + ent.lastName + ", " + ent.firstName + '</li>');
+                $app.find('ul').append('<li data-id="' + ent.id + '"' + ' data-ab="' + ent.addressBookId + '">' + ent.lastName + ", " + ent.firstName + '</li>');
             });
 
             $app.find('li').on('click', function() {
-                var addressBookId = $(this).data('id');
-                displayEntry(addressBookId);
+                var entryId = $(this).data('id');
+                var addressBookId = $(this).data('ab');
+                displayEntry(entryId);
+                entryButtons(addressBookId);
             });
         }
     )
@@ -83,27 +86,83 @@ console.log(entries)
 
 }
 
-function displayEntry() {
-    getEntry().then(
+function displayEntry(id) {
+    getEntry(id).then(
         function(entryInfo) {
-
             $app.html(''); // Clear the #app div
             $app.append('<h2>Contact Information:</h2>');
-            $app.append('<ul></ul>');
-<<<<<<< HEAD
-
-            $app.find('ul').append('<li>' + "First Name: " + entryInfo[0].firstName + '</li>');
-            $app.find('ul').append('<li>' + "Lirst Name: " + entryInfo[0].lastName + '</li>');
-            $app.find('ul').append('<li>' + "Birthday: " + entryInfo[0].birthday + '</li>');
-            $app.find('ul').append('<li>' + "id: " + entryInfo[0].id + '</li>');
-            $app.find('ul').append('<li>' + "addressBookId: " + entryInfo[0].addressBookId + '</li>');
-            $app.find('ul').append('<li id="addresses">' + "address(es): " + '</li>');
-            entryInfo[0].addresses.forEach(function(ab) {
-                $app.find('#addresses').append('<li data-id="' + ab.id + '">' + ab.name + '</li>');
+            $app.append('<ul id="contact"></ul>');
+            $app.find('#contact').append('<li>' + "First Name: " + entryInfo[0].firstName + '</li>');
+            $app.find('#contact').append('<li>' + "Lirst Name: " + entryInfo[0].lastName + '</li>');
+            $app.find('#contact').append('<li>' + "Birthday: " + entryInfo[0].birthday + '</li>');
+            // $app.find('ul').append('<li>' + "id: " + entryInfo[0].id + '</li>');
+            // $app.find('ul').append('<li>' + "addressBookId: " + entryInfo[0].addressBookId + '</li>');
+            $app.find('#contact').append('<ul id="addresses">' + "address(es): " + '</ul>');
+            entryInfo[0].addresses.forEach(function(ad, ind) {
+                $app.find('#addresses').append('<li>  Address #' + ind+1 + '</li>');
+                $app.find('#addresses').append('<li>' + "line1 :" + ad.line1 + '</li>');
+                $app.find('#addresses').append('<li>' + "line2 :" + ad.line2 + '</li>');
+                $app.find('#addresses').append('<li>' + "city :" + ad.city + '</li>');
+                $app.find('#addresses').append('<li>' + "state :" + ad.state + '</li>');
+                $app.find('#addresses').append('<li>' + "zip :" + ad.zip + '</li>');
+                $app.find('#addresses').append('<li>' + "country :" + ad.country + '</li>');
+                $app.find('#addresses').append('<li>' + "type :" + ad.type + '</li>');
             });
+            $app.find('#contact').append('<ul id="phones">' + "phone(s): " + '</ul>');
+             entryInfo[0].phones.forEach(function(ph, ind) {
+                $app.find('#phones').append('<li>  Phone #' + ind+1 + '</li>');
+                $app.find('#phones').append('<li>' + "Phone Number :" + ph.phoneNumber + '</li>');
+                $app.find('#phones').append('<li>' + "Type :" + ph.type + '</li>');
+                $app.find('#phones').append('<li>' + "Phone Type :" + ph.phoneType + '</li>');
+            });
+             $app.find('#contact').append('<ul id="emais">' + "email(s): " + '</ul>');
+             entryInfo[0].emails.forEach(function(em, ind) {
+                $app.find('#emails').append('<li>  Email #' + ind+1 + '</li>');
+                $app.find('#emails').append('<li>' + "Email Address :" + em.email + '</li>');
+                $app.find('#emails').append('<li>' + "Type :" + em.type + '</li>');
+            });
+        }
+    );
+    
+}
+
+// End functions that display views
 
 
-        })
+// Start the app by displaying all the addressbooks
+
+
+
+
+function AddressBooksListButtons() {
+    // var offset = 0
+    // $next.html(''); // Clear the #next div
+    // $prev.html(''); // Clear the #prev div
+    // $next.text("Display Next 5");
+    // $prev.text("Display Prev 5");
+    var offset = 0
+    $buttons.html('');
+    // $buttons.append('<button id="bookPrev">Display Prev 5</button>');
+    // $buttons.append('<button id="bookNext">Display Next 5</button>');
+    $buttons.append('<button id="entryPrev">Display Prev 5</button>');
+    $buttons.append('<button id="entryNext">Display Next 5</button>');
+    // $("#entryNext").text("Display Next 5");
+    // $("#entryPrev").text("Display Prev 5");
+    $("#entryNext").on('click', function() {
+    // $("#entryPrev").on('click', function() {
+    
+    // $("#bookNext").on('click', function() {
+        offset +=5
+        displayAddressBooksList(offset);
+    });
+    $("#entryPrev").on('click', function() {
+    // $("#bookPrev").on('click', function() {
+        offset -=5
+        if (offset < 0) {
+            offset = 0;
+        }
+        displayAddressBooksList(offset);
+    });
 }
 // End functions that display views
 
@@ -112,53 +171,46 @@ function displayEntry() {
 
 // AddressBooksListButtons();
 
-
-function AddressBooksListButtons() {
-    $next.html(''); // Clear the #next div
-    $prev.html(''); // Clear the #prev div
-    $next.text("Display Next 5");
-    $prev.text("Display Prev 5");
-    $next.on('click', function() {
-        var $skip = $app.find('li:last-child')
-        var $id = $skip.data('id')
-        return displayAddressBooksList($id);
-    })
-    $prev.on('click', function() {
-        var $skip = $app.find('li:first-child')
-        var $id = $skip.data('id');
-        return displayAddressBooksList($id - 5);
-    })
-
-<<<<<<< HEAD
+function AddressBookEntriesButtons(addressBookId) {
+    var offset = 0
+    $buttons.html('');
+    $buttons.append('<button id="entryPrev">Display Prev 5</button>');
+    $buttons.append('<button id="entryNext">Display Next 5</button>');
+    // $("#entryNext").text("Display Next 5");
+    // $("#entryPrev").text("Display Prev 5");
+    $("#entryNext").on('click', function() {
+        offset +=5
+        displayAddressBookEntries(addressBookId, offset);
+    });
+    $("#entryPrev").on('click', function() {
+        offset -=5
+        if (offset < 0) {
+            offset = 0;
+        }
+        displayAddressBookEntries(addressBookId, offset);
+    });
 }
-=======
-        })
+
+
+function entryButtons(addressBookId) {
+    console.log(addressBookId)
+    $buttons.html('');
+    $buttons.append('<button id="bookList">Return to Address Book List</button>');
+    $buttons.append('<button id="entryList">Return to Entry Listing</button>');
+    // $("#entryNext").text("Display Next 5");
+    // $("#entryPrev").text("Display Prev 5");
+    $("#bookList").on('click', function() {
+        AddressBooksListButtons()
+        displayAddressBooksList(0)
+    });
+    $("#entryList").on('click', function() {
+        AddressBookEntriesButtons(addressBookId);
+        displayAddressBookEntries(addressBookId, 0);
+    });
 }
-// End functions that display views
 
-
-// Start the app by displaying all the addressbooks
-
-// AddressBooksListButtons();
-
-function AddressBooksListButtons() {
-    $next.html(''); // Clear the #next div
-    $prev.html(''); // Clear the #prev div
-    $next.text("Display Next 5");
-    $prev.text("Display Prev 5");
-    $next.on('click', function() {
-        var $skip = $app.find('li:last-child')
-        var $id = $skip.data('id')
-        return displayAddressBooksList($id);
-    })
-    $prev.on('click', function() {
-        var $skip = $app.find('li:first-child')
-        var $id = $skip.data('id');
-        return displayAddressBooksList($id - 5);
-    })
-
-}
->>>>>>> 8ae660ccee62912c4f6223e49a3c982e77dc8d10
 
 displayAddressBooksList(0);
-AddressBooksListButtons()
+
+AddressBooksListButtons();
+
